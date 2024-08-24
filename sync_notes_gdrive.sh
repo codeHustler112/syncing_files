1425304b0655df9b54864102ccabf5758dada70d
fin <fin@debian>
2024-08-24 19:56:48 +0530

    changes made

--- sync_notes_gdrive.sh
+++ sync_notes_gdrive.sh
@@ -6,5 +6,10 @@ REMOTE_DIR=remote:sync_notes             #Replace remote:sync_notes with your co
 #sync local files to drive
 rclone sync "$LOCAL_DIR" "$REMOTE_DIR"
 
+#send notifications with environment variables
+DISPLAY=:0
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+export DISPLAY DBUS_SESSION_BUS_ADDRESS
+
 #send notifications
 notify-send "Sync Complete" "Your files have been synched."
